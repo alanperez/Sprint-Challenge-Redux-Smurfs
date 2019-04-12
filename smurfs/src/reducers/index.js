9fcd26b4525ee1797e2d1ee56197663451ef29ca
Alan Perez <alanp9six@gmail.com>
2019-04-12 13:51:55 -0500

    MVP completed.. adding smurf and deleting smurf

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,61 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import {
+  FETCHING_SMURFS,
+  SUCCESS,
+  ERROR,
+  ADDING_SMURF,
+  DELETING_SMURF
+} from "../actions";
+
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: ''
+};
+
+function smurfsReducer(state = initialState, action) {
+  // console.log(smurfsReducer, action);
+  switch (action.type) {
+    case FETCHING_SMURFS:
+      return {
+        ...state,
+        fetchingSmurfs: true,
+        error: ''
+      };
+    case SUCCESS:
+      return {
+        ...state,
+        smurfs: action.payload,
+        error: ''
+      };
+    case ERROR:
+      return {
+        ...state,
+        error: action.payload
+      };
+    case ADDING_SMURF:
+      return {
+        ...state,
+        addingSmurf: true,
+        error: ''
+      };
+    case DELETING_SMURF:
+      return {
+        ...state,
+        deletingSmurf: true,
+        error: ''
+      };
+    default:
+      return state;
+  }
+}
+
+export default smurfsReducer;
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
